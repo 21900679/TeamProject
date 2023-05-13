fe239a86de06e4e798c7535c50b030721c884fde
21900679 <grapes612@naver.com>
2023-05-13 18:23:21 +0900

    create selectMenu

--- moneyfolio.c
+++ moneyfolio.c
@@ -24,4 +24,21 @@ int selectIE(){
     scanf("%d", &IE);
     return IE;
 }
-int selectMenu();
+int selectMenu(){
+    int menu;
+    printf("┌────────── MoneyFolio ─────────────┐\n");
+    printf("|    1. 조회                        |\n");
+    printf("|    2. 수입/지출 내역 추가         |\n");
+    printf("|    3. 고정(수입/지출) 내역 추가   |\n");
+    printf("|    4. 수입/지출 내역 검색         |\n");
+    printf("|    5. 분류별 검색                 |\n");
+    printf("|    6. 수입/지출 내역 수정         |\n");
+    printf("|    7. 수입/지출 내역 삭제         |\n");
+    printf("|    8. 일일 내역 조회              |\n");
+    printf("|    9. 월별 내역 조회              |\n");
+    printf("|    10. 저장하기                   |\n");
+    printf("└───────────────────────────────────┘\n");
+    printf("원하는 메뉴를 선택하시오. ");
+    scanf("%d", &menu);
+    return menu;
+}
