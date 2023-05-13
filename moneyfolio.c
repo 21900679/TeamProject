a45f543b72a1bf1f2a792e5baa35e59d1f2fa65f
21900679 <grapes612@naver.com>
2023-05-13 18:34:38 +0900

    함수 정의 수정

--- moneyfolio.c
+++ moneyfolio.c
@@ -11,13 +11,13 @@ void updateExpense(Expense *ex);
 int deleteIncome(Income *in);
 int deleteExpense(Expense *ex);
 
-void sumList(Income *in, Expense *ex);
-int searchCategory(Income * in, Expense *ex);
-int searchIncome(Income *in);
-int searchExpense(Expense *ex);
-void dayList(Income * in, Expense *ex);
-void monthList(Income * in, Expense *ex);
-int save(Income * in, Expense *ex);
+void sumList(Income *in[], Expense *ex[]);
+int searchCategory(Income * in[], Expense *ex[]);
+int searchIncome(Income *in[]);
+int searchExpense(Expense *ex[]);
+void dayList(Income *in[], Expense *ex[]);
+void monthList(Income *in[], Expense *ex[]);
+int save(Income *in[], Expense *ex[]);
 int selectIE(){
     int IE;
     printf("수입(1), 지출(2)을 선택해주세요.\n");
