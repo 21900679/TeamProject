af2e19b56ba3ad4341c2213b5f71f0202f7b6f32
21900679 <grapes612@naver.com>
2023-05-16 21:38:06 +0900

    serachIncome 기능 구현

--- moneyfolio.c
+++ moneyfolio.c
@@ -13,14 +13,38 @@ int deleteExpense(Expense *ex);
 
 void sumList(Income *in[], Expense *ex[]);
 int searchCategory(Income * in[], Expense *ex[]);
-int searchIncome(Income *in[]);
+
+int searchIncome(Income *in[], int countIn){         // 수입내역 검색 기능
+    char serach[100];
+    int month = 0;
+    int day = 0;
+    int pmonth = 0;
+    int pday = 0;
+    printf("검색하실 내용을 입력해 주세요. ");
+    scanf("%s", serach);
+
+    for(int i = 0; i < countIn; i++){
+        month = in[i]->date[0] * 10 + in[i]->date[1];
+        day = in[i]->date[2] * 10 + in[i]->date[3];
+        if(strstr(in[i]->note, serach) != NULL){
+            if(pmonth != month)
+                printf("%d월 %d일-------------------------------\n", month, day);
+            printf("%s %s %d\n", InCate[in[i]->category], in[i]->note, in[i]->price);
+            if(pmonth != month)
+                printf("----------------------------------------\n");
+        }
+        pmonth = month;
+        pday = day;
+    }
+}  
+
 int searchExpense(Expense *ex[]);
 void dayList(Income *in[], Expense *ex[]);
 void monthList(Income *in[], Expense *ex[]);
 int save(Income *in[], Expense *ex[]);
 int selectIE(){
     int IE;
-    printf("수입(1), 지출(2)을 선택해주세요.\n");
+    printf("수입(1), 지출(2)을 선택해주세요. ");
     scanf("%d", &IE);
     return IE;
 }
