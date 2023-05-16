0af843bf18632351a58d65f779134c5ed88e6c81
21900679 <grapes612@naver.com>
2023-05-16 21:37:00 +0900

    category 분류 array추가

--- moneyfolio.h
+++ moneyfolio.h
@@ -11,6 +11,9 @@ typedef struct{
         int category;
 } Expense;      // 지출
 
+char InCate[3][10] = {"월급","부수입","용돈"};
+char ExCate[3][20] = {"식비","생활용품","교통/차량"};
+
 int addIncome(Income *in);		                  // 수입내역 추가
 int addExpense(Expense *ex);		                // 지출내역 추가
 int addfIncome(Income *in);		                  // 고정수입내역 추가
@@ -23,7 +26,7 @@ int deleteExpense(Expense *ex); 	              // 지출내역 삭제
 
 void sumList(Income *in[], Expense *ex[]);          // 일별 내역 list + 합계 출력 기능
 int searchCategory(Income *in[], Expense *ex[]);           // 지출 & 수입 분류별 검색 기능
-int searchIncome(Income *in[]);                   // 수입내역 검색 기능
+int searchIncome(Income *in[], int countIn);                   // 수입내역 검색 기능
 int searchExpense(Expense *ex[]);                 // 지출내역 검색 기능
 void dayList(Income *in[], Expense *ex[]);         // 일일 내역 list 기능
 void monthList(Income *in[], Expense *ex[]);       // 월별 내역 list 기능
