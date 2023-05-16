3ec376a6e50153089e1118de2de9ef3afcbf2ac4
21900679 <grapes612@naver.com>
2023-05-17 03:31:00 +0900

    함수 정의 수정

--- moneyfolio.h
+++ moneyfolio.h
@@ -19,15 +19,15 @@ void updateExpense(Moneyfolio *mf);             // 지출내역 수정
 int deleteIncome(Moneyfolio *mf);               // 수입내역 삭제
 int deleteExpense(Moneyfolio *mf);              // 지출내역 삭제
 
-void searchCateEx(Moneyfolio *mf[], int countEx);               // 지출 분류별 검색 기능
-void searchCateIn(Moneyfolio *mf[], int countIn);               // 수입 분류별 검색 기능
-void searchIncome(Moneyfolio *mf[], int countIn);               // 수입내역 검색 기능
-void searchExpense(Moneyfolio *mf[], int countEx);              // 지출내역 검색 기능
+void searchCateEx(Moneyfolio *mf[], int total);               // 지출 분류별 검색 기능
+void searchCateIn(Moneyfolio *mf[], int total);               // 수입 분류별 검색 기능
+void searchIncome(Moneyfolio *mf[], int total);               // 수입내역 검색 기능
+void searchExpense(Moneyfolio *mf[], int ctotal);              // 지출내역 검색 기능
 
-void dayList(Moneyfolio *mf[]);                 // 일일 내역 list 기능
-void monthList(Moneyfolio *mf[]);               // 월별 내역 list 기능
-void sumList(Moneyfolio *mf[]);                 // 일별 내역 list + 합계 출력 기능
-int save(Moneyfolio *mf[]);                     // 일별 내역 list +  합계 저장기능
+void dayList(Moneyfolio *mf[], int total);                 // 일일 내역 list 기능
+void monthList(Moneyfolio *mf[], int total);               // 월별 내역 list 기능
+void sumList(Moneyfolio *mf[], int total);                 // 일별 내역 list + 합계 출력 기능
+void save(Moneyfolio *mf[], int total);                     // 일별 내역 list +  합계 저장기능
 
 int selectIE();                                 // 수입, 지출 고르는 기능
 int selectMenu();                               // 메뉴 고르는 기능
